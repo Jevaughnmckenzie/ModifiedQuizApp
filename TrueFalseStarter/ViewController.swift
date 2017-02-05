61085a7e6fb59c1db57981309d2cbaf0eacf3ce8
Jevaughn Mckenzie <jmckenzie1004@gmail.com>
2017-02-04 22:23:56 -0500

    Added timer function

--- TrueFalseStarter/ViewController.swift
+++ TrueFalseStarter/ViewController.swift
@@ -187,7 +187,18 @@ class ViewController: UIViewController {
         nextRound()
     }
     
-    
+    func lightningModeTimer(seconds: Int) {
+        // Converts a delay in seconds to nanoseconds as signed 64 bit integer
+        let delay = Int64(NSEC_PER_SEC * UInt64(seconds))
+        // Calculates a time value to execute the method given current time and delay
+        let dispatchTime = DispatchTime.now() + Double(delay) / Double(NSEC_PER_SEC)
+        
+        // Executes the nextRound method at the dispatch time on the main queue
+        DispatchQueue.main.asyncAfter(deadline: dispatchTime) {
+            
+            self.noAnswer()
+        }
+    }
 
     
     // MARK: Helper Methods
