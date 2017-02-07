f12c846e2a052f3101c891dbf26e396c45dfbf1b
Jevaughn Mckenzie <jmckenzie1004@gmail.com>
2017-02-06 23:08:48 -0500

    Updated Questions

--- TrueFalseStarter/Questions.swift
+++ TrueFalseStarter/Questions.swift
@@ -21,12 +21,12 @@ class TriviaQuestion {
     let question: String
     let answer: String
     let wrongAnswer1: String
-    let wrongAnswer2: String
+    var wrongAnswer2: String? = nil
     var wrongAnswer3: String? = nil
     let answerChoices: [String?]
     
     
-    init(question: String, answer: String, wrongAnswer1: String, wrongAnswer2: String, wrongAnswer3: String?){
+    init(question: String, answer: String, wrongAnswer1: String, wrongAnswer2: String?, wrongAnswer3: String?){
         self.question = question
         self.answer = answer
         self.wrongAnswer1 = wrongAnswer1
@@ -43,13 +43,15 @@ class TriviaQuestion {
 
 let question1 = TriviaQuestion(question: "Who was Marcus Garvey?", answer: "A renowned Black Nationalist", wrongAnswer1: "A blacksmith", wrongAnswer2: "A gospel singer", wrongAnswer3: "A painter")
 
-let question2 = TriviaQuestion(question: "Who dropped the most moving rap album of the 21st century?", answer: "Kendrick Lamar", wrongAnswer1: "Taylor Swift", wrongAnswer2: "Lil Yahty", wrongAnswer3: nil)
+let question2 = TriviaQuestion(question: "What was the name of the secret FBI taskforce that tried to discredit the Civil Rights Movement?", answer: "COINTELPRO", wrongAnswer1: "No such thing.", wrongAnswer2: "INFILTRATORS", wrongAnswer3: "The Black Panthers")
 
 let question3 = TriviaQuestion(question: "How much do Black women make in comparison to each dollar their white male coworkers earn?", answer: "64 cents", wrongAnswer1: "70 cents", wrongAnswer2: "84 cents", wrongAnswer3: "75 cents")
 
 let question4 = TriviaQuestion(question: "How many seats in Congress are held by women?", answer: "17%", wrongAnswer1: "34%", wrongAnswer2: "40%", wrongAnswer3: "25%")
 
-let question5 = TriviaQuestion(question: "What is the term used to describe feminism that places a special emphasis on marginalized women groups?", answer: "Womanism", wrongAnswer1: "Lesbians", wrongAnswer2: "man-hater", wrongAnswer3: "Communist")
+let question5 = TriviaQuestion(question: "What is the term that was coined to describe feminism that places a special emphasis on marginalized women groups?", answer: "Womanism", wrongAnswer1: "Lesbians", wrongAnswer2: "man-haters", wrongAnswer3: "Communists")
+
+let question6 = TriviaQuestion(question: "How can a person come to understand someone else?", answer: "Listen to them.", wrongAnswer1: "Interrupt and talk over them.", wrongAnswer2: nil, wrongAnswer3: nil)
 
 
 
@@ -58,7 +60,8 @@ let trivia: [TriviaQuestion] = [
     question2,
     question3,
     question4,
-    question5
+    question5,
+    question6
 ]
 
 // sets the question order for the given round
